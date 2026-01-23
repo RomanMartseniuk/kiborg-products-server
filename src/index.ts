34e36b26c34b95f1941ab53d92cc3d2524dd9892
Roman Martseniuk <martseniuk.roma@gmail.com>
2026-01-23 17:01:03 +0200

    start dev

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 const express = require("express");
+import type { Request, Response } from "express";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.get("/status", (req: Request, res: Response) => {
+  res.send('Server is running...')
 });
 
 app.listen(3000, "0.0.0.0", () => {
