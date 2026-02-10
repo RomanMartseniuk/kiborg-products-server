82d6a240f2696bf162d1a40b8c7887eab4c6aec6
Roman Martseniuk <martseniuk.roma@gmail.com>
2026-02-10 16:37:15 +0200

    complete function for fetching data to database

---
+++ src/utils/updateLists.ts
@@ -0,0 +1,13 @@
+import { CategoryRepository } from "../database/CategoriesRepository";
+import { ProductRepository } from "../database/ProductsRepository ";
+import { fetchData } from "../services/fetchData";
+
+export async function updateLists() {
+   console.log('Start Updating DB...');
+   const {products, categories} = await fetchData();
+
+   CategoryRepository.insertMany(categories);
+
+   ProductRepository.insertMany(products);
+   console.log('DB successfully updated');
+}
